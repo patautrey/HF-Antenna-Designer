8f59a1dfc1fb9606694ee6e10181875f1925744d
patautrey <patautrey@gmail.com>
2026-06-09 13:41:35 -0500

    Update index.js

--- ui/panels/index.js
+++ ui/panels/index.js
@@ -1,11 +1,3 @@
-/* ============================================================
-   UI Panel Registry
-   ============================================================ */
-
-import FlowerpotPanel from "/ui/panels/flowerpot-panel.js";
-import FlowerpotDiagramPanel from "/ui/panels/flowerpot-diagram-panel.js";
-import FlowerpotBuildSheetPanel from "/ui/panels/flowerpot-buildsheet-panel.js";
-import FlowerpotMultibandPanel from "/ui/panels/flowerpot-multiband-panel.js";
 import FlowerpotPerformancePanel from "/ui/panels/flowerpot-performance-panel.js";
 
 export const PANEL_MAP = {
