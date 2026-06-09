0628f32defd623fbf18d319c7be07f0f3b6a7ec1
patautrey <patautrey@gmail.com>
2026-06-09 13:26:27 -0500

    Create antenna-selector.js

---
+++ ui/components/antenna-selector.js
@@ -0,0 +1,12 @@
+/* ============================================================
+   Antenna Selector Component
+   ============================================================ */
+
+export function antennaSelector() {
+    return `
+    <select id="antenna_select">
+        <option value="flowerpot">Flowerpot (T2LT)</option>
+        <option value="flowerpot-diagram">Flowerpot Diagram</option>
+    </select>
+    `;
+}
