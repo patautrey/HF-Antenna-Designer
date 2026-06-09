0bb422fcc482458974a91993ab458090f0e17550
patautrey <patautrey@gmail.com>
2026-06-09 13:38:11 -0500

    Update index.js

--- ui/panels/index.js
+++ ui/panels/index.js
@@ -6,10 +6,12 @@ import FlowerpotPanel from "/ui/panels/flowerpot-panel.js";
 import FlowerpotDiagramPanel from "/ui/panels/flowerpot-diagram-panel.js";
 import FlowerpotBuildSheetPanel from "/ui/panels/flowerpot-buildsheet-panel.js";
 import FlowerpotMultibandPanel from "/ui/panels/flowerpot-multiband-panel.js";
+import FlowerpotPerformancePanel from "/ui/panels/flowerpot-performance-panel.js";
 
 export const PANEL_MAP = {
     "flowerpot": FlowerpotPanel,
     "flowerpot-diagram": FlowerpotDiagramPanel,
     "flowerpot-buildsheet": FlowerpotBuildSheetPanel,
-    "flowerpot-multiband": FlowerpotMultibandPanel
+    "flowerpot-multiband": FlowerpotMultibandPanel,
+    "flowerpot-performance": FlowerpotPerformancePanel
 };
