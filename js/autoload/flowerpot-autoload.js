8f5f5b8aa2c93dcb427ac42ff79c1c875cb81429
patautrey <patautrey@gmail.com>
2026-06-09 16:05:38 -0500

    Create flowerpot-autoload.js

---
+++ js/autoload/flowerpot-autoload.js
@@ -0,0 +1,9 @@
+/* ---------------------------------------------------------
+   HF Workbench — Flowerpot Auto‑Registration
+--------------------------------------------------------- */
+
+import { loadFlowerpotBundle } from "../bundles/flowerpot-bundle.js";
+
+export function autoloadFlowerpot(app) {
+    loadFlowerpotBundle(app);
+}
