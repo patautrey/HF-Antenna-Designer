00a8e7b5edd5e8f8b04532735a5bd987ed71993e
patautrey <patautrey@gmail.com>
2026-06-09 13:46:57 -0500

    Update index.js

--- ui/panels/index.js
+++ ui/panels/index.js
@@ -1,9 +1,14 @@
-import FlowerpotPerformancePanel from "/ui/panels/flowerpot-performance-panel.js";
+import FlowerpotFieldPanel from "/ui/panels/flowerpot-field-panel.js";
+import FlowerpotHeightPanel from "/ui/panels/flowerpot-height-panel.js";
+import FlowerpotCoaxLossPanel from "/ui/panels/flowerpot-coaxloss-panel.js";
 
 export const PANEL_MAP = {
     "flowerpot": FlowerpotPanel,
     "flowerpot-diagram": FlowerpotDiagramPanel,
     "flowerpot-buildsheet": FlowerpotBuildSheetPanel,
     "flowerpot-multiband": FlowerpotMultibandPanel,
-    "flowerpot-performance": FlowerpotPerformancePanel
+    "flowerpot-performance": FlowerpotPerformancePanel,
+    "flowerpot-field": FlowerpotFieldPanel,
+    "flowerpot-height": FlowerpotHeightPanel,
+    "flowerpot-coaxloss": FlowerpotCoaxLossPanel
 };
