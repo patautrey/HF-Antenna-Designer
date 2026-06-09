422c3a6f7997b54b562b7e9a3ca4501806963d50
patautrey <patautrey@gmail.com>
2026-06-09 13:35:17 -0500

    Update antenna-selector.js

--- ui/components/antenna-selector.js
+++ ui/components/antenna-selector.js
@@ -8,6 +8,7 @@ export function antennaSelector() {
         <option value="flowerpot">Flowerpot (T2LT)</option>
         <option value="flowerpot-diagram">Flowerpot Diagram</option>
         <option value="flowerpot-buildsheet">Flowerpot Build Sheet</option>
+        <option value="flowerpot-multiband">Multi‑Band Flowerpot Designer</option>
     </select>
     `;
 }
