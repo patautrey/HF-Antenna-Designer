7c28d45c8081c0ebc3cbed7a917e9bafee3f789f
patautrey <patautrey@gmail.com>
2026-06-09 13:25:53 -0500

    Update index.js

--- ui/panels/index.js
+++ ui/panels/index.js
@@ -1,5 +1,11 @@
+/* ============================================================
+   UI Panel Registry
+   ============================================================ */
+
 import FlowerpotPanel from "/ui/panels/flowerpot-panel.js";
+import FlowerpotDiagramPanel from "/ui/panels/flowerpot-diagram-panel.js";
 
 export const PANEL_MAP = {
     "flowerpot": FlowerpotPanel,
+    "flowerpot-diagram": FlowerpotDiagramPanel
 };
