d6251b0b7a81aae365387d4cb11382c7a25c587d
patautrey <patautrey@gmail.com>
2026-06-10 11:39:35 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,14 +1,7 @@
-// HF-Antenna-Designer/app/app.js
-// Registry Mode — Full Replacement File
+// app/app.js
 
-import flowerpot from "../modules/module-flowerpot.js";
-import invertedV from "../modules/module-inverted-v.js";
-import yagi3 from "../modules/module-vertical-yagi3.js";
+import flowerpot from "../engines/flowerpot.js";
 
-const app = {
-    flowerpot,
-    invertedV,
-    yagi3
+export default {
+    flowerpot
 };
-
-export default app;
