e526c279164c5508854ba43d101ff32ac996968b
patautrey <patautrey@gmail.com>
2026-06-10 12:00:31 -0500

    Update antenna-selector.js

--- ui/antenna-selector.js
+++ ui/antenna-selector.js
@@ -1,10 +1,10 @@
-// ui/antenna-selector.js
-
 export default function antennaSelector() {
     return `
-        <label for="antenna_select">Select Antenna Type</label>
+        <label for="antenna_select">Select Antenna:</label>
         <select id="antenna_select">
             <option value="flowerpot">Flowerpot</option>
+            <option value="invertedV">Inverted-V</option>
+            <option value="yagi3">Vertical Yagi 3-Element</option>
         </select>
     `;
 }
