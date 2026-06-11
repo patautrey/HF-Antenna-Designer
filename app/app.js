4c5d2685ffa98a4c5e8c3aed16780ef324c9ed80
patautrey <patautrey@gmail.com>
2026-06-11 09:20:11 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,25 +3,25 @@
 
 // ENGINE IMPORTS
 import FlowerpotEngine from "../engines/FlowerpotEngine.js";
-import InvertedvEngine from "../engines/InvertedvEngine.js";
+import InvertedVEngine from "../engines/InvertedVEngine.js";
 import Yagi3Engine from "../engines/Yagi3Engine.js";
 
 // PANEL IMPORTS
 import FlowerpotPanel from "../ui/panels/FlowerpotPanel.js";
-import InvertedvPanel from "../ui/panels/InvertedvPanel.js";
+import InvertedVPanel from "../ui/panels/InvertedVPanel.js";
 import Yagi3Panel from "../ui/panels/Yagi3Panel.js";
 
 // REGISTRY
 const app = {
     engines: {
         flowerpot: (config) => new FlowerpotEngine(config),
-        invertedv: (config) => new InvertedvEngine(config),
+        invertedv: (config) => new InvertedVEngine(config),
         yagi3: (config) => new Yagi3Engine(config)
     },
 
     panels: {
         flowerpot: FlowerpotPanel,
-        invertedv: InvertedvPanel,
+        invertedv: InvertedVPanel,
         yagi3: Yagi3Panel
     },
 
