4fb5b347d78d6f0b2f34c2c7bebe3796d86b74bc
patautrey <patautrey@gmail.com>
2026-06-11 14:15:34 -0500

    Update index.js

--- ui/panels/index.js
+++ ui/panels/index.js
@@ -1,15 +1,15 @@
 // HF-Antenna-Designer/ui/panels/index.js
 
-import FlowerpotPanel from "./flowerpot.js";
-import DipolePanel from "./dipole.js";
-import EndFedPanel from "./endFed.js";
-import LoopPanel from "./loop.js";
-import QuadPanel from "./quad.js";
-import MoxonPanel from "./moxon.js";
-import VerticalYagiPanel from "./verticalYagi.js";
-import JPolePanel from "./jPole.js";
-import InvertedVPanel from "./invertedV.js";
-import Yagi3Panel from "./yagi3.js";
+import FlowerpotPanel from "./FlowerpotPanel.js";
+import DipolePanel from "./DipolePanel.js";
+import EndFedPanel from "./EndFedPanel.js";
+import LoopPanel from "./LoopPanel.js";
+import QuadPanel from "./QuadPanel.js";
+import MoxonPanel from "./MoxonPanel.js";
+import VerticalYagiPanel from "./VerticalYagiPanel.js";
+import JPolePanel from "./JPolePanel.js";
+import InvertedVPanel from "./InvertedVPanel.js";
+import Yagi3Panel from "./Yagi3Panel.js";
 
 export const panels = {
     flowerpot: FlowerpotPanel,
