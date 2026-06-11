4ef0822ea0035503b3725f6277c44afdaa951084
patautrey <patautrey@gmail.com>
2026-06-11 09:14:12 -0500

    Update FlowerpotPanel.js

--- ui/panels/FlowerpotPanel.js
+++ ui/panels/FlowerpotPanel.js
@@ -1,12 +1,14 @@
+// ui/panels/FlowerpotPanel.js
+
 export default function FlowerpotPanel(runSimulation) {
     return `
-        <h2>Flowerpot Configuration</h2>
+        <h2>Flowerpot Antenna Configuration</h2>
 
-        <label>Frequency (MHz)</label>
-        <input id="freq" value="146">
+        <label for="freq">Frequency (MHz)</label>
+        <input id="freq" type="number" value="146">
 
-        <label>Velocity Factor</label>
-        <input id="vf" value="0.66">
+        <label for="vf">Velocity Factor</label>
+        <input id="vf" type="number" step="0.01" value="0.66">
 
         <button id="runBtn">Run Simulation</button>
     `;
