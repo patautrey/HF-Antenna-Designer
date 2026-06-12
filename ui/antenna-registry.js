df9519cd69f50058d92cbd787fa8a0955ea40b2f
patautrey <patautrey@gmail.com>
2026-06-12 16:11:41 -0500

    Update antenna-registry.js

--- ui/antenna-registry.js
+++ ui/antenna-registry.js
@@ -5,8 +5,6 @@ import flowerpot from "../modules/necgen-flowerpot.js";
 import g5rv from "../modules/necgen-g5rv.js";
 import groundplane from "../modules/necgen-groundplane.js";
 
-// Add more antennas here as you create modules
-
 export const antennaRegistry = {
   invertedV,
   flowerpot,
