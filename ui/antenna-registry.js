d760dfd2280ff1bc3a816602227a037c718dff8c
patautrey <patautrey@gmail.com>
2026-06-12 13:44:05 -0500

    Update antenna-registry.js

--- ui/antenna-registry.js
+++ ui/antenna-registry.js
@@ -1,17 +1,16 @@
 export const antennaRegistry = {
   flowerpot: {
     label: "Flowerpot (T2LT)",
-    module: () => import("../modules/module-flowerpot.js"),
-    json: "./content/antennas/flowerpot.json",
-    params: { frequencyMHz: 27.2, velocityFactor: 0.66 },
-    diagram: () => import("../ui/diagrams/flowerpot-diagram.js")
-  },
-
-  invertedV: {
-    label: "Inverted V",
-    module: () => import("../modules/module-inverted-v.js"),
-    json: "./content/antennas/inverted-v.json",
-    params: { frequencyMHz: 7.1 },
-    diagram: null
+    necDeck: `
+CM Flowerpot T2LT
+CE
+GW 1 21 0 0 0 0 0 5.456 0.001
+GE 0
+GN 2 0 0 0 13 0.005
+EX 0 1 11 0 1 0
+FR 0 1 0 0 27.2 0
+RP 0 181 1 1000 0 0 1 1
+EN
+`
   }
 };
