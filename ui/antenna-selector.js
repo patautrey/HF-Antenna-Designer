f4f6242d59468fc5cf1d0faabc3dced4a19dd379
patautrey <patautrey@gmail.com>
2026-06-12 11:36:01 -0500

    Update antenna-selector.js

--- ui/antenna-selector.js
+++ ui/antenna-selector.js
@@ -1,17 +1,14 @@
+import { antennaRegistry } from "./antenna-registry.js";
+
 export default function antennaSelector() {
-    return `
-<label for="antenna_select">Select Antenna:</label>
-<select id="antenna_select">
-    <option value="flowerpot">Flowerpot (T2LT)</option>
-    <option value="dipole">Dipole</option>
-    <option value="endFed">End-Fed</option>
-    <option value="loop">Loop</option>
-    <option value="quad">Quad</option>
-    <option value="moxon">Moxon</option>
-    <option value="verticalYagi">Vertical Yagi</option>
-    <option value="jPole">J-Pole</option>
-    <option value="invertedV">Inverted V</option>
-    <option value="yagi3">3-Element Yagi</option>
-</select>
-`;
+  const options = Object.entries(antennaRegistry)
+    .map(([key, entry]) => `<option value="${key}">${entry.label}</option>`)
+    .join("");
+
+  return `
+    <label for="antenna_select">Select Antenna:</label>
+    <select id="antenna_select">
+      ${options}
+    </select>
+  `;
 }
